ed841cb0c14ce18c80430ba18066dcbf510dbc37
bauer01 <bauer01@seznam.cz>
2011-05-01 18:36:04 +0200

    added plugin for CTRL + "KEY" events & added CTRL + A (mark all items) [closes #19]

--- www/fm-src/js/core.js
+++ www/fm-src/js/core.js
@@ -60,4 +60,20 @@
                 });
         }
 
+        $.ctrl = function(key, callback, args) {
+            $(document).keydown(function(e) {
+                if(!args) args=[]; // IE barks when args is null
+                if(e.keyCode == key.charCodeAt(0) && e.ctrlKey) {
+                    callback.apply(this, args);
+                    return false;
+                }
+            });
+        };
+
+        $.fn.markAll = function(tag, clickedClass) {
+            this.children(tag).each(function() {
+                $(this).addClass(clickedClass);
+            });
+        };
+
 })(jQuery);
