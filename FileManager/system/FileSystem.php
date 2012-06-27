47e2c70e7a36eef6aa0a016653eadf496f74c0e4
Bauer01 <bronislav.sedlak@gmail.com>
2012-06-27 20:28:27 +0200

    refactoring: function remove_diacritic replaced with Nette function toAscii

--- FileManager/system/FileSystem.php
+++ FileManager/system/FileSystem.php
@@ -504,104 +504,6 @@ class FileSystem
 
 
         /**
-         * Remove file name diacritic
-         * @param string $string
-         * @return string
-         */
-        public function remove_diacritic($string)
-        {
-            $charset = Array(
-              'ä'=>'a',
-              'Ä'=>'A',
-              'á'=>'a',
-              'Á'=>'A',
-              'à'=>'a',
-              'À'=>'A',
-              'ã'=>'a',
-              'Ã'=>'A',
-              'â'=>'a',
-              'Â'=>'A',
-              'č'=>'c',
-              'Č'=>'C',
-              'ć'=>'c',
-              'Ć'=>'C',
-              'ď'=>'d',
-              'Ď'=>'D',
-              'ě'=>'e',
-              'Ě'=>'E',
-              'é'=>'e',
-              'É'=>'E',
-              'ë'=>'e',
-              'Ë'=>'E',
-              'è'=>'e',
-              'È'=>'E',
-              'ê'=>'e',
-              'Ê'=>'E',
-              'í'=>'i',
-              'Í'=>'I',
-              'ï'=>'i',
-              'Ï'=>'I',
-              'ì'=>'i',
-              'Ì'=>'I',
-              'î'=>'i',
-              'Î'=>'I',
-              'ľ'=>'l',
-              'Ľ'=>'L',
-              'ĺ'=>'l',
-              'Ĺ'=>'L',
-              'ń'=>'n',
-              'Ń'=>'N',
-              'ň'=>'n',
-              'Ň'=>'N',
-              'ñ'=>'n',
-              'Ñ'=>'N',
-              'ó'=>'o',
-              'Ó'=>'O',
-              'ö'=>'o',
-              'Ö'=>'O',
-              'ô'=>'o',
-              'Ô'=>'O',
-              'ò'=>'o',
-              'Ò'=>'O',
-              'õ'=>'o',
-              'Õ'=>'O',
-              'ő'=>'o',
-              'Ő'=>'O',
-              'ř'=>'r',
-              'Ř'=>'R',
-              'ŕ'=>'r',
-              'Ŕ'=>'R',
-              'š'=>'s',
-              'Š'=>'S',
-              'ś'=>'s',
-              'Ś'=>'S',
-              'ť'=>'t',
-              'Ť'=>'T',
-              'ú'=>'u',
-              'Ú'=>'U',
-              'ů'=>'u',
-              'Ů'=>'U',
-              'ü'=>'u',
-              'Ü'=>'U',
-              'ù'=>'u',
-              'Ù'=>'U',
-              'ũ'=>'u',
-              'Ũ'=>'U',
-              'û'=>'u',
-              'Û'=>'U',
-              'ý'=>'y',
-              'Ý'=>'Y',
-              'ž'=>'z',
-              'Ž'=>'Z',
-              'ź'=>'z',
-              'Ź'=>'Z'
-            );
-
-            return strtr($string, $charset);
-        }
-
-
-        /**
          * Get safe folder name
          * 
          * @param string $name
@@ -617,7 +519,7 @@ class FileSystem
                 elseif (str_replace(array('.', ' '), '', $name) == "")  # because of this: .. .
                         $name = "";
 
-                return $this->remove_diacritic($name);
+                return \Nette\Utils\Strings::toAscii($name);
         }
 
 
@@ -631,7 +533,7 @@ class FileSystem
                 $except = array("\\", "/", ":", "*", "?", '"', "<", ">", "|");
                 $name = str_replace($except, "", $name);
 
-                return $this->remove_diacritic($name);
+                return \Nette\Utils\Strings::toAscii($name);
         }
 
 
