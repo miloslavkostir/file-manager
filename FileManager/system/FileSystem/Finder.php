1051224690a2b6d891ce209eabfbe0cab0474038
bauer01 <bronislav.sedlak@gmail.com>
2012-10-01 14:59:42 +0200

    typos

--- FileManager/system/FileSystem/Finder.php
+++ FileManager/system/FileSystem/Finder.php
@@ -14,8 +14,10 @@ class Finder extends \Nette\Utils\Finder
 
     /**
      * Sets the order comparison function
+     *
      * @param callback $order
-     * @return Finder
+     *
+     * @return \Ixtrum\FileManager\Application\FileSystem
      */
     public function order($order)
     {
@@ -26,43 +28,44 @@ class Finder extends \Nette\Utils\Finder
     public function orderBy($key)
     {
         switch ($key) {
-            case 'name':
+            case "name":
                 $this->order = function($f1, $f2) {
                             return \strcasecmp($f1->getFilename(), $f2->getFilename());
                         };
                 break;
-            case 'type':
+            case "type":
                 $this->order = function($f1, $f2) {
                             return \strcasecmp(
                                             pathinfo($f1->getPathName(), PATHINFO_EXTENSION), pathinfo($f2->getPathName(), PATHINFO_EXTENSION));
                         };
                 break;
-            case 'time':
+            case "time":
                 $this->order = function($f1, $f2) {
                             return $f2->getMTime() - $f1->getMTime();
                         };
                 break;
-            case 'size':
+            case "size":
                 $this->order = function($f1, $f2) {
                             $fileSystem = new FileSystem;
                             return $fileSystem->filesize($f2->getPathName()) - $fileSystem->filesize($f1->getPathName());
                         };
                 break;
             default:
-                throw new InvalidArgumentException('Unknown expression, allowed are NAME, TYPE, TIME, SIZE');
+                throw new InvalidArgumentException("Unknown expression, allowed are NAME, TYPE, TIME, SIZE");
         }
 
         return $this;
     }
 
     /**
-     * Returns iterator.
+     * Get iterator
+     *
      * @return \Iterator
      */
     public function getIterator()
     {
         $iterator = parent::getIterator();
-        if ($this->order === NULL) {
+        if ($this->order === null) {
             return $iterator;
         }
 
