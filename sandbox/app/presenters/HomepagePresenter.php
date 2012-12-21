ddad5ff01ca9a9224fd9295fdd54550d0a9c7f82
Bronislav Sedlák <bauer@bauer-debian.workgroup>
2012-12-21 20:53:27 +0100

    sandbox: added res dir autosync

--- sandbox/app/presenters/HomepagePresenter.php
+++ sandbox/app/presenters/HomepagePresenter.php
@@ -6,6 +6,7 @@ class HomepagePresenter extends Nette\Application\UI\Presenter
     public function createComponentFileManager()
     {
         $config = array(
+            "syncResDir" => true,
             "uploadroot" => $this->context->parameters["wwwDir"] . "/data"
         );
         return new Ixtrum\FileManager($this->context, $config);
