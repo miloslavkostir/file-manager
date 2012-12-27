727cf61006c56d994af79c9108a3950ea3fe8f2b
Bauer01 <bronislav.sedlak@gmail.com>
2012-12-27 13:02:39 +0100

    controls: Rename code standard

--- FileManager/application/controls/Rename/Rename.php
+++ FileManager/application/controls/Rename/Rename.php
@@ -9,35 +9,42 @@ class Rename extends \Ixtrum\FileManager\Application\Controls
 
     public function render()
     {
-        $file = "";
-        if (isset($this->selectedFiles[0])) {
-            $file = $this->selectedFiles[0];
-        }
         $this->template->setFile(__DIR__ . "/Rename.latte");
         $this->template->setTranslator($this->system->translator);
-        $this->template->origFile = $file;
-
-        $this->getComponent("renameForm")->setDefaults(array(
-            "new_filename" => $file,
-            "orig_filename" => $file,
-        ));
-
         $this->template->render();
     }
 
+    /**
+     * RenameForm component factory
+     *
+     * @return \Nette\Application\UI\Form
+     */
     protected function createComponentRenameForm()
     {
         $form = new Form;
         $form->setTranslator($this->system->translator);
-        $form->addText("new_filename", "New name")
+        $form->addText("newFilename", "New name")
                 ->setRequired("New name required.");
-        $form->addHidden("orig_filename");
+        $form->addHidden("originalFilename");
         $form->addSubmit("send", "OK");
+
+        if (isset($this->selectedFiles[0])) {
+            $form["newFilename"]->setDefaultValue($this->selectedFiles[0]);
+            $form["originalFilename"]->setDefaultValue($this->selectedFiles[0]);
+        }
+
         $form->onSuccess[] = $this->renameFormSuccess;
         $form->onError[] = $this->parent->parent->onFormError;
         return $form;
     }
 
+    /**
+     * RenameForm success event
+     *
+     * @param \Nette\Application\UI\Form $form RenameForm
+     *
+     * @return void
+     */
     public function renameFormSuccess(Form $form)
     {
         $actualDir = $this->getActualDir();
@@ -48,25 +55,25 @@ class Rename extends \Ixtrum\FileManager\Application\Controls
             return;
         }
 
-        if ($form->values->new_filename === $form->values->orig_filename) {
+        if ($form->values->newFilename === $form->values->originalFilename) {
             $this->parent->parent->flashMessage($this->system->translator->translate("New name can not be the same!"), "warning");
             return;
         }
 
-        if (file_exists($path . DIRECTORY_SEPARATOR . $form->values->new_filename)) {
-            $this->parent->parent->flashMessage($this->system->translator->translate("The name %s was already used.", $form->values->new_filename), "warning");
+        if (file_exists($path . DIRECTORY_SEPARATOR . $form->values->newFilename)) {
+            $this->parent->parent->flashMessage($this->system->translator->translate("The name %s was already used.", $form->values->newFilename), "warning");
             return;
         }
 
-        if (!$this->isPathValid($actualDir, $form->values->orig_filename)) {
-            $this->parent->parent->flashMessage($this->system->translator->translate("File/folder %s does not already exists!", $form->values->orig_filename), "error");
+        if (!$this->isPathValid($actualDir, $form->values->originalFilename)) {
+            $this->parent->parent->flashMessage($this->system->translator->translate("File/folder %s does not already exists!", $form->values->originalFilename), "error");
             return;
         }
 
-        $origPath = $path . DIRECTORY_SEPARATOR . $form->values->orig_filename;
+        $origPath = $path . DIRECTORY_SEPARATOR . $form->values->originalFilename;
         if (is_dir($origPath)) {
 
-            $newFilename = $this->system->filesystem->safeFoldername($form->values->new_filename);
+            $newFilename = $this->system->filesystem->safeFoldername($form->values->newFilename);
             $this->system->thumbs->deleteDirThumbs($origPath);
 
             if ($this->system->parameters["cache"]) {
@@ -76,7 +83,7 @@ class Rename extends \Ixtrum\FileManager\Application\Controls
             }
         } else {
 
-            $newFilename = $this->system->filesystem->safeFilename($form->values->new_filename);
+            $newFilename = $this->system->filesystem->safeFilename($form->values->newFilename);
             $this->system->thumbs->deleteThumb($origPath);
 
             if ($this->system->parameters["cache"]) {
@@ -89,7 +96,7 @@ class Rename extends \Ixtrum\FileManager\Application\Controls
             $this->parent->parent->flashMessage($this->system->translator->translate("Successfully renamed to %s.", $newFilename), "info");
             $this->system->session->clear("clipboard");
         } else {
-            $this->parent->parent->flashMessage($this->system->translator->translate("An error occurred during %s rename!", $form->values->orig_filename), "error");
+            $this->parent->parent->flashMessage($this->system->translator->translate("An error occurred during %s rename!", $form->values->originalFilename), "error");
         }
     }
 
