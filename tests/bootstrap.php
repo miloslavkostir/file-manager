6c6505ba86754c4c020604eb133f4d2ba122ac85
Bronislav Sedlák <bronislav.sedlak@gmail.com>
2013-03-04 16:29:26 +0100

    tests: fixed wrong path in robotloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 $loader = new Nette\Loaders\RobotLoader;
-$loader->addDirectory(__DIR__ . "/../FileManager")
+$loader->addDirectory(__DIR__ . "/../src")
         ->addDirectory(__DIR__ . "/cases")
         ->setCacheStorage(new Nette\Caching\Storages\DevNullStorage)
         ->register();
