5bc1b24df39150ebd8d47cb48b9e462ce382e191
Bauer01 <bronislav.sedlak@gmail.com>
2013-03-07 22:52:45 +0100

    FileManager: removed useless syncResources() method - should be done externally

--- src/FileManager.php
+++ src/FileManager.php
@@ -265,31 +265,6 @@ class FileManager extends \Nette\Application\UI\Control
     }
 
     /**
-     * Synchronize all resources file manager & it's plugin resources to
-     * defined resource directory located in web root.
-     */
-    public function syncResources()
-    {
-        $sep = DIRECTORY_SEPARATOR;
-
-        // Copy main resources
-        $this->system->filesystem->copyDir(
-                realpath($this->system->parameters["appDir"] . "{$sep}resources"), $this->system->parameters["wwwDir"] . $sep . $this->system->parameters["resDir"]
-        );
-
-        // Copy plugins resources
-        foreach ($this->system->parameters["plugins"] as $plugin) {
-
-            if (isset($plugin["resources"]) && !empty($plugin["resources"])) {
-
-                $from = $plugin["path"] . $sep . $plugin["resources"];
-                $to = $this->system->parameters["wwwDir"] . $sep . $this->system->parameters["resDir"] . "{$sep}plugins{$sep}" . $plugin["name"];
-                $this->system->filesystem->copyDir($from, $to);
-            }
-        }
-    }
-
-    /**
      * Path validator
      *
      * @param string $dir  Dirname as relative path
