eb7db278f2c61d580bca6b36f5c4fadcaed32711
Bauer01 <bronislav.sedlak@gmail.com>
2013-03-29 14:10:59 +0100

    translator: added pretty json export

--- src/application/Translator.php
+++ src/application/Translator.php
@@ -75,8 +75,68 @@ class Translator implements \Nette\Localization\ITranslator
     public function __destruct()
     {
         if ($this->extract) {
-            file_put_contents($this->langFile, json_encode($this->dictionary));
+            file_put_contents($this->langFile, self::indent(json_encode($this->dictionary)));
         }
+    }
+
+    /**
+     * Indents a flat JSON string to make it more human-readable.
+     *
+     * @link http://www.daveperrett.com/articles/2008/03/11/format-json-with-php
+     *
+     * @param string $json The original JSON string to process.
+     *
+     * @return string Indented version of the original JSON string.
+     */
+    public static function indent($json)
+    {
+        $result = '';
+        $pos = 0;
+        $strLen = strlen($json);
+        $indentStr = '    ';
+        $newLine = "\n";
+        $prevChar = '';
+        $outOfQuotes = true;
+
+        for ($i = 0; $i <= $strLen; $i++) {
+
+            // Grab the next character in the string.
+            $char = substr($json, $i, 1);
+
+            // Are we inside a quoted string?
+            if ($char == '"' && $prevChar != '\\') {
+                $outOfQuotes = !$outOfQuotes;
+
+                // If this character is the end of an element,
+                // output a new line and indent the next line.
+            } else if (($char == '}' || $char == ']') && $outOfQuotes) {
+                $result .= $newLine;
+                $pos--;
+                for ($j = 0; $j < $pos; $j++) {
+                    $result .= $indentStr;
+                }
+            }
+
+            // Add the character to the result string.
+            $result .= $char;
+
+            // If the last character was the beginning of an element,
+            // output a new line and indent the next line.
+            if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+                $result .= $newLine;
+                if ($char == '{' || $char == '[') {
+                    $pos++;
+                }
+
+                for ($j = 0; $j < $pos; $j++) {
+                    $result .= $indentStr;
+                }
+            }
+
+            $prevChar = $char;
+        }
+
+        return $result;
     }
 
 }
